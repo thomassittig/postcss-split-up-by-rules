41ef2b44fde7ce7256c60a8a34d8eff653db5647
Thomas Sittig <thomas.sittig@googlemail.com>
2017-06-23 13:01:56 +0200

    initial rough version

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
 var postcss = require('postcss');
+/**
+ * Split a single rule definition into two
+ *
+ */
 
-module.exports = postcss.plugin('PLUGIN_NAME', function (opts) {
-    opts = opts || {};
+let handleForContext = (selector, originRule, root) =>{
+    let regexSelector = new RegExp(`(${selector}\\s{1})`, 'g'),
+        skipNext = false;
 
+    if (originRule.selector.indexOf(selector) === 0) {
+        let themedRule = originRule.clone(), dcls = [];
+        themedRule.removeAll();
+
+        originRule.walkDecls('color', (decl) =>{
+            dcls.push(decl);
+            decl.remove();
+        });
+
+        originRule.walkDecls('background-color', (decl) =>{
+            dcls.push(decl);
+            decl.remove();
+        });
+
+        originRule.selector = originRule.selector.replace(regexSelector, '');
+
+        dcls.map((dcl) =>{
+            themedRule.append(dcl);
+        });
+
+        root.insertAfter(originRule, themedRule);
+        skipNext = true;
+
+        console.log('duplicated: ', themedRule.selector, ' => ', originRule.selector)
+    }
+
+    return skipNext;
+}
+
+module.exports = postcss.plugin('postcss-split-up-by-rules', function (opts){
+    opts = Object.assign({}, {listenFor: [], extract: []}, opts || {});
     // Work with options here
 
-    return function (root, result) {
-
+    return function (root, result){
         // Transform CSS AST here
-
-    };
+        opts.listenFor.forEach((selector) =>{
+            let skipNext = false;
+            root.walkRules(function (originRule){
+                if (skipNext) {
+                    skipNext = false;
+                } else {
+                    skipNext = handleForContext(selector, originRule, root)
+                }
+            })
+        })
+    }
 });
